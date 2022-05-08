2977096051203a1af7e5ea6352206cf7f23d4c49
Darion01 <dariusgh01@yahoo.com>
2022-05-08 19:34:34 +0300

    Update CartController.java

--- src/main/java/platform/webapplication/controller/CartController.java
+++ src/main/java/platform/webapplication/controller/CartController.java
@@ -1,4 +1,49 @@
-package platform.webapplication.controller;
+package platform.webapplication.service;
 
-public class CartController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import platform.webapplication.model.Cart;
+import platform.webapplication.model.Order;
+import platform.webapplication.repository.CartRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CartService {
+
+    private CartRepository cartRepository;
+
+    @Autowired
+    public CartService(CartRepository cartRepository){
+        this.cartRepository = cartRepository;
+    }
+    public List<Cart> findAll() {
+
+        var it = cartRepository.findAll();
+
+        ArrayList<Cart> carts = new ArrayList<Cart>();
+        it.forEach(e -> carts.add(e));
+
+        return carts;
+    }
+
+    public Cart findById(Integer id)
+    {
+        var cart = cartRepository.findById(id);
+        return cart.orElse(null);
+    }
+
+    public Long count() {
+
+        return cartRepository.count();
+    }
+
+    public void deleteById(Integer cartId) {
+        cartRepository.deleteById(cartId);
+    }
+
+    public Cart saveToCart(Cart cart){
+        return cartRepository.save(cart);
+    }
 }
