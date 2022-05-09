7048b019bc69853748abd23202cc805282608525
13212312312 <nejneriumihai@gmail.com>
2022-05-09 18:09:52 +0300

    Update Product.java

--- src/main/java/platform/webapplication/entities/Product.java
+++ src/main/java/platform/webapplication/entities/Product.java
@@ -25,23 +25,22 @@ public class Product {
     @NotNull(message = "Price should not be null!")
     private Float price;
     private Date date;
-    private Integer stock;
     @NotNull(message = "Year should not be null!")
     private Integer year;
     @NotNull(message = "Strap should not be null!")
-    private Integer strap;
-    private Integer glass;
+    private String strap;
+    private String glass;
     @NotNull(message = "Strap color should not be null!")
-    private Integer strap_color;
-    private Integer water_resistence;
-    private Integer carcase;
-    private Integer carcase_form;
-    private Integer carcase_color;
-    private Integer carcase_thickness;
+    private String strap_color;
+    private String water_resistence;
+    private String carcase;
+    private String carcase_form;
+    private String carcase_color;
+    private String carcase_thickness;
     private Byte alarm;
     private Byte timer;
     @NotNull(message = "Mechanism should not be null!")
-    private Integer mechanism;
+    private String mechanism;
     private Integer rating;
     @NotNull(message = "Gender should not be null!")
     private Byte gen;
@@ -50,9 +49,9 @@ public class Product {
     private Byte promovat;
     private String category;
     @NotNull(message = "Brand should not be null!")
-    private Integer brand;
+    private String brand;
     @NotNull(message = "Model should not be null!")
-    private Integer model;
+    private String model;
 
     @Override
     public boolean equals(Object o) {
