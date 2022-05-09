ffb4fdea7b207bbbd1fe1bf8fdd4b21455fa896a
Oana024 <mocanuoana52@yahoo.com>
2022-05-09 18:06:32 +0300

    Edit Models

--- src/main/java/platform/webapplication/models/Cart/CartDeleted.java
+++ src/main/java/platform/webapplication/models/Cart/CartDeleted.java
@@ -2,6 +2,8 @@ package platform.webapplication.models.Cart;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -11,4 +13,17 @@ public class CartDeleted {
     private Integer cartId;
     private String error = "";
     private int statusCode = 500;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartDeleted that = (CartDeleted) o;
+        return statusCode == that.statusCode && Objects.equals(cartId, that.cartId) && Objects.equals(error, that.error);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cartId, error, statusCode);
+    }
 }
--- src/main/java/platform/webapplication/models/Products/SingleProduct.java
+++ src/main/java/platform/webapplication/models/Products/SingleProduct.java
@@ -3,6 +3,8 @@ package platform.webapplication.models.Products;
 import lombok.*;
 import platform.webapplication.entities.Product;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -13,4 +15,16 @@ public class SingleProduct {
     private String error = "";
     private int statusCode = 500;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SingleProduct that = (SingleProduct) o;
+        return statusCode == that.statusCode && Objects.equals(product, that.product) && Objects.equals(error, that.error);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, error, statusCode);
+    }
 }
