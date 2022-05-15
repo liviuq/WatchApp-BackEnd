9b502233eb3972e3e240877237c48880b00e9924
liviuq <petrache.andrei1@gmail.com>
2022-05-15 14:05:11 +0300

    Please, before editing a word, check if that word exists in the dictionary.

--- src/main/java/platform/webapplication/entities/Product.java
+++ src/main/java/platform/webapplication/entities/Product.java
@@ -20,8 +20,8 @@ public class Product {
     private Integer id;
     @NotNull(message = "User id should not be null!")
     private Integer user_id;
-    @NotNull(message = "Name should not be null!")
-    private String name;
+//    @NotNull(message = "Name should not be null!")
+//    private String name;
     @NotNull(message = "Price should not be null!")
     private Float price;
     private Date date;
@@ -43,11 +43,11 @@ public class Product {
     private String mechanism;
     private Integer rating;
     @NotNull(message = "Gender should not be null!")
-    private Byte gen;
-    private Byte promovat;
+    private Byte gender;
+    private Byte promovated;
     private String category;
-    @NotNull(message = "Brand should not be null!")
-    private String brand;
+//    @NotNull(message = "Brand should not be null!")
+//    private String brand;
     @NotNull(message = "Model should not be null!")
     private String model;
 
